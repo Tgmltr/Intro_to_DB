1e74ea0636dbce5ee7e9a4f9e882df8ade38d4c9
Etaguemalter <eh.taguemalter@gmail.com>
2024-10-21 00:02:32 +0200

    UPDATED FILE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -37,5 +37,5 @@ book_id INT NOT NULL,
 quantity DOUBLE,
 PRIMARY KEY(orderdetailid),
 FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-FOREIGN KEY (book_id ) REFERENCES Books(book_id )
+FOREIGN KEY (book_id) REFERENCES Books(book_id )
 );
