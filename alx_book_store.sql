dc4f8e504b4e74b6641d4b9538d6137ca87c883b
Etaguemalter <eh.taguemalter@gmail.com>
2024-10-20 23:59:16 +0200

    UPDATED FILE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,13 +1,13 @@
 
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE Authors(
+CREATE TABLE IF NOT EXISTS Authors(
 author_id INT NOT NULL PRIMARY KEY,
 author_name VARCHAR(215)
 );
 
-CREATE TABLE Books(
+CREATE TABLE IF NOT EXISTS Books(
 book_id INT NOT NULL PRIMARY KEY,
 title VARCHAR(130),
 author_id INT NOT NULL,
@@ -16,21 +16,21 @@ publication_date DATE,
 FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Customers(
+CREATE TABLE IF NOT EXISTS Customers(
 customer_id INT NOT NULL ,
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT,
 PRIMARY KEY (customer_id)
 );
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXISTS Orders(
 order_id INT NOT NULL,
 customer_id INT NOT NULL,
 order_date DATE,
 PRIMARY KEY(order_id),
 FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
 orderdetailid INT NOT NULL,
 order_id INT NOT NULL,
 book_id INT NOT NULL,
